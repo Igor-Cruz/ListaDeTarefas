25ae0fc2959e66e6a8a9d3f61d9a55432dd2d8ee
Igor-Cruz <igorcruz.it@outlook.com>
2021-07-06 20:10:29 -0300

    criando as conclusões de tarefas

--- js/main.js
+++ js/main.js
@@ -5,20 +5,27 @@ const Main = {
   },
 
   cacheSelectors: function () {
-    this.checkButtons = document.querySelectorAll('.check')
+    this.$checkButtons = document.querySelectorAll('.check')
   },
 
   bindEvents: function () {
     const self = this
 
-    this.checkButtons.forEach(function (button) {
+    this.$checkButtons.forEach(function (button) {
       button.onclick = self.Events.checkButton_click
     })
   },
 
   Events: {
-    checkButton_click: function () {
-      alert('ok')
+    checkButton_click: function (e) {
+      const li = e.target.parentElement
+      const isDONE = li.classList.contains('done')
+
+      if (isDONE) {
+        li.classList.remove('done')
+      } else {
+        li.classList.add('done')
+      }
     }
   }
 }
